e1f555245083c3896980d983a01e299884102ac5
Eytan Canzini <ecanzini1@sheffield.ac.uk>
2021-03-15 17:31:11 +0000

    Testing bluetooth config

--- MyProject/main.c
+++ MyProject/main.c
@@ -7,6 +7,23 @@
 #include "hal.h"
 #include "memory_protection.h"
 #include <main.h>
+#include "leds.h"
+#include "spi_comm.h"
+#include "motors.h"
+#include "epuck1x/uart/e_uart_char.h"
+#include "stdio.h"
+#include "serial_comm.h"
+
+
+void clear_leds(void);
+void spi_comm_start(void);
+void set_led(led_name_t led_number, unsigned int value);
+void motors_init(void);
+void left_motor_set_speed(int motor_speed);
+void right_motor_set_speed(int motor_speed);
+void serial_start(void);
+void e_send_uart1_char(const char * buff, int buff_len);
+
 
 
 int main(void)
@@ -15,11 +32,25 @@ int main(void)
     halInit();
     chSysInit();
     mpu_init();
+    clear_leds();
+    spi_comm_start();
+    motors_init();
 
+    left_motor_set_speed(0);
+    right_motor_set_speed(0);
+    serial_start();
+
+    char str[100];
+    int str_length;
+    str_length = sprintf(str, "Hello World!\n");
 
     /* Infinite loop. */
     while (1) {
+    	set_led(LED1, 1);
     	//waits 1 second
+    	e_send_uart1_char(str, str_length);
+        chThdSleepMilliseconds(1000);
+        set_led(LED1, 0);
         chThdSleepMilliseconds(1000);
     }
 }
